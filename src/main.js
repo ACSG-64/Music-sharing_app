ddf3c2897778bdcdd804a53d41cb3e28a63599ab
Andrés Sarmiento <acamilo707@gmail.com>
2021-11-12 22:39:45 -0500

    i18n and PWA plugins added to the project globally

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import router from './router';
 import store from './store';
 import VeeValidatePlugin from './includes/validation';
 import { auth } from './includes/firebase';
+import Icon from './directives/icon';
 import './assets/tailwind.css';
 import './assets/main.css';
+import i18n from './includes/i18n';
+import './registerServiceWorker';
 
 let app;
 
@@ -13,9 +16,12 @@ auth.onAuthStateChanged(() => {
   if (!app) {
     app = createApp(App);
 
+    app.use(i18n);
     app.use(store);
     app.use(router);
     app.use(VeeValidatePlugin);
+
+    app.directive('icon', Icon);
 
     app.mount('#app');
   }
